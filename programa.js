3ba7ee5b4525cc2d1266005d938af0c0d0ecae9f
hugocrocco <hugoacrocco@gmail.com>
2025-01-25 07:05:54 -0300

    logica

--- programa.js
+++ programa.js
@@ -1,1 +1,20 @@
-Programa.js
+let listaAmigos=[];
+
+do 
+{
+let amigo=prompt("ingrese el nombre de su amigo o amiga");
+if (amigo)
+    {
+        listaAmigos.push(amigo);
+    }
+    let opcion=prompt("desea agregar a otro amigo? (si/no)").toLowerCase();
+
+}
+while (opcion==="si");
+console.log("lista de amigos");
+
+listaAmigos.forEach((amigo), index)=>{console.log(`${index},${amigo}`)};
+
+
+
+
